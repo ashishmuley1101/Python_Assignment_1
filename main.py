1ab050af3192eba482ca839980114ec18f8fed29
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-13 20:46:54 +0530

    Python program to calculate simple interest.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
-# Python program to convert Celsius to Fahrenheit .
+# Python program to calculate simple interest.
 
-celsius = float(input("Enter Temperature in Celsius : "))
+principal_amount = float(input("Enter the Principal Amount : "))
+rate_interest = float(input("Enter the Rate of Interest in % : "))
+time = float(input("Enter the Time(in year) for amount leading : "))
 
-print("Temperature :", celsius, "°C")
+simple_interest = (principal_amount * rate_interest * time) / 100
 
-fahrenheit = celsius * 1.8 + 32
-
-print("Temperature in Fahrenheit :", round(fahrenheit, 2), "°F")
+print("Simple Interest for", principal_amount, "Rs.", "with rate interest", rate_interest, "% for",
+    time, "year is",simple_interest, "Rs.")
